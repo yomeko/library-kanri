9e50a7076a826808dfd6f78561a6aaf061932d0d
mid <mid@k302.kcsk.ac.jp>
2025-12-02 09:47:48 +0900

    hama

--- src/main/java/model_Logic/newAcountLogic.java
+++ src/main/java/model_Logic/newAcountLogic.java
@@ -3,7 +3,7 @@ package model_Logic;
 import model.User;
 
 public class newAcountLogic {
-	public boolean execute(User user) {
+	public boolean create(User user) {
 		//登録処理（実際の登録処理は行わない）
 		return true;
 	}
