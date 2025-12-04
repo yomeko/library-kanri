84ec5273377c372f9e54bf46bb02738e7c351473
yuyukiti <mintoayumu1005@icloud.com>
2025-12-04 13:17:41 +0900

    no

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -8,14 +8,14 @@ public class User {
 	private String pass;
 	
 	public User() {}
-	public User(String name, String id, String pass) {
+	public User(String name, /*String id,*/ String pass) {
 		this.name = name;
-		this.id = id;
+		//this.id = id;
 		this.pass = pass;
 	}
 	
 	public String getName() {return name;}
-	public String getId() {return id;}
+	//public String getId() {return id;}
     public String getPass() {return pass;}
 	
 }
--- src/main/java/servlet/newAcount.java
+++ src/main/java/servlet/newAcount.java
@@ -8,6 +8,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import model.User;
 
 @WebServlet("/newAcount")
