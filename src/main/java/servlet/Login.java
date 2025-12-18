c72cb34f8e3a3d6b726cbcda4ad852d806461fca
mid <mid@k302.kcsk.ac.jp>
2025-12-18 10:21:27 +0900

    hara

--- src/main/java/servlet/Login.java
+++ src/main/java/servlet/Login.java
@@ -67,8 +67,8 @@ public class Login extends HttpServlet {
         // ログイン結果画面の表示用
         request.setAttribute("isLogin", isLogin);
         request.setAttribute("username", username);
-        
-        System.out.println(isLogin + " lougin.java dopost");
+
+	System.out.println(isLogin + " lougin.java dopost");
         System.out.println(username + " lougin.java dopost");
 
         RequestDispatcher dispatcher =
